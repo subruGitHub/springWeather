2c1a7eb17cefd390584d42eb958611ed2bd81c35
Subru <msubru@gmail.com>
2021-06-27 00:26:20 +0530

    jenkins initial

--- src/main/java/com/example/springWeather/SpringWeatherApplication.java
+++ src/main/java/com/example/springWeather/SpringWeatherApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringWeatherApplication {
 
 	public static void main(String[] args) {
