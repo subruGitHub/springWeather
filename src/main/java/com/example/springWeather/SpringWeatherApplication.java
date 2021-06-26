93975ca8297c10e433e7aeb48bef8793c7ef323a
Subru <msubru@gmail.com>
2021-06-27 03:03:16 +0530

    jenkins initial

--- src/main/java/com/example/springWeather/SpringWeatherApplication.java
+++ src/main/java/com/example/springWeather/SpringWeatherApplication.java
@@ -2,15 +2,23 @@ package com.example.springWeather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringWeatherApplication  {
+public class SpringWeatherApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringWeatherApplication.class);
+	}
+
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringWeatherApplication .class, args);
 	}
 
